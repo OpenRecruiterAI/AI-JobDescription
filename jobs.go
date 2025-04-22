9f16cb9c9d0237a755a1804b0ac379b6a79419b8
piccotalent192@gmail.com <piccotalent192@gmail.com>
2025-04-22 19:43:42 +0530

    added generating job description from file function

--- jobs.go
+++ jobs.go
@@ -5,7 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func JobSetup(config Config) *Job {
@@ -42,4 +46,52 @@ func GenerateJobDescriptionFromTitle(jobTitle, apiEndpoint string) (responseStri
 
 	return string(respBody), nil
 
+}
+
+func GenerateJobDescriptionFromFile() {
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	filePart, err := writer.CreateFormFile("file", jdFileHeader.Filename)
+	if err != nil {
+		fmt.Println("err getting file : ", err)
+		c.JSON(200, gin.H{"err": "err creating request to the node api", "success": 0, "response": ""})
+	}
+
+	_, err = jdFile.Seek(0, io.SeekStart)
+	if err != nil {
+		fmt.Println("Error resetting file pointer:", err)
+		c.JSON(200, gin.H{"err": "Error resetting file pointer", "success": 0, "response": ""})
+	}
+
+	Js, err := io.Copy(filePart, jdFile)
+	fmt.Println("Js,", Js)
+	if err != nil {
+		fmt.Println("couldn't write file data:", err)
+		c.JSON(200, gin.H{"err": "couldn't write file data", "success": 0, "response": ""})
+	}
+
+	writer.Close()
+
+	baseurl := os.Getenv("CHAT_API_URL") + "api/recruit/analyse?type=jobDetail"
+
+	resp, err := http.Post(baseurl, writer.FormDataContentType(), body)
+	if err != nil {
+		fmt.Println("failed to get data from api", err)
+		c.JSON(200, gin.H{"err": "failed to get data from api", "success": 0, "response": ""})
+	}
+
+	if resp.StatusCode == 200 {
+		bodyResp, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("failed to read the response from the body", err)
+			c.JSON(200, gin.H{"err": "failed to read the response from the body", "success": 0, "response": ""})
+		}
+
+		c.JSON(200, gin.H{"err": "", "success": 1, "response": string(bodyResp)})
+
+	} else {
+		c.JSON(200, gin.H{"err": "failed to get response from the api", "success": 0, "response": ""})
+	}
 }
